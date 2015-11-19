77ade0245e70e9063f72598557189242807e349c
mikeleeviel <mikelee10@live.nl>
2015-11-19 13:11:13 +0100

    Create start.hx

---
+++ start.hx
@@ -0,0 +1,19 @@
+class Main 
+{
+	
+	static function main() 
+	{
+		//story (start)
+		Sys.println ("Once up on a time... or another you lived,");
+		Sys.println ("wait... who are you?");
+		var name : String = Sys.stdin().readLine();
+		Sys.println ("Hi " +name+", mankind has been attacked by the cyborg wizard!");
+		Sys.println ("you must go back in time to find the means to stop him");
+		Sys.println ("you are our only hope");
+		
+		// TODO connect to image etc.
+		var doorKey : Bool = false;
+		var chestkey : Bool = false;
+	}
+	
+}
